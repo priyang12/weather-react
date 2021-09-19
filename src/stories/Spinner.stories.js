8c4e9bee62560cb6ee01922b0b94546d884fc05f
Gambino <patelpriyang95@gmail.com>
2021-09-20 02:47:58 +0530

    Final

--- src/stories/Spinner.stories.js
+++
@@ -1,9 +0,0 @@
-import Spinner from '../Utilis/Spinner';
-
-export default {
-  title: 'Spinner',
-  component: Spinner,
-};
-const Template = (args) => <Spinner {...args} />;
-
-export const Default = Template.bind({});
