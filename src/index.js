59b7138236e5f5a89e5195910b70aa557ba6a4f9
priyang patel <72823974+priyang12@users.noreply.github.com>
2022-02-09 19:16:22 +0530

    Fixed Data

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './styles/Global.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import ReactGA from "react-ga";
+import "./styles/Global.css";
+
+const TRACKING_ID = process.env.REACT_APP_AnaKey;
+console.log(TRACKING_ID);
+ReactGA.initialize(TRACKING_ID);
+
+ReactGA.event({
+  category: "User",
+  action: "Created an Account",
+});
+ReactGA.exception({
+  description: "An error ocurred",
+  fatal: true,
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 reportWebVitals();
